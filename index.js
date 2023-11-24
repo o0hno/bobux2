adc167fcdfa3128da12cb91f374f49ac94f6540d
pon <dinissufianov8@gmail.com>
2023-11-24 15:19:15 +0500

    wwwwww

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/gif', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'gif'));
 });
 
-app.post('/upload', (req, res) => {
+app.post('/', (req, res) => {
   const gifName = req.body.gifName;
 
   if (!req.files || Object.keys(req.files).length === 0) {
