e92ab3e3c3242f2823de75b780e942484cfdd3a6
o0hno <86475123+o0hno@users.noreply.github.com>
2024-02-10 15:56:32 +0500

    12

--- index.php
+++ index.php
@@ -3,75 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Загрузка файлов</title>
+    <link rel="icon" href="/1.ico" type="image/x-icon">
+    <title>hi</title>
     <style>
+
+
+        @keyframes fadeIn {
+            from {
+                opacity: 0;
+            }
+            to {
+                opacity: 1;
+            }
+        }
         body {
             font-family: Arial, sans-serif;
+            background-color: #181818;
             margin: 0;
             padding: 0;
-            background-color: #181818;
-            color: #fff;
-
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            font-family: Arial, sans-serif;
             text-align: center;
         }
-        .container {
-            background-color: #2c2c2c;
-            max-width: 600px;
-            margin: 50px auto;
-            padding: 20px;
 
+        .container {
+            text-align: center;
+        }
+
+        h1 {
+            color: #ffffff;
+        }
+
+        p {
+            color: #fff;
+        }
+
+        .message {
+            background-color: #2c2c2c;
+            padding: 20px;
             border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0,0,0,0.1);
-        }
-        h2 {
-            margin-bottom: 20px;
-        }
-        form {
-            margin-bottom: 20px;
-        }
-        input[type="file"], input[type="text"], input[type="submit"] {
-            margin-bottom: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
     </style>
 </head>
 <body>
     <div class="container">
-        <h2>Загрузка файлов</h2>
-        <form id="uploadForm" action="upload.php" method="post" enctype="multipart/form-data">
-            <label for="fileToUpload">Выберите файл для загрузки:</label><br>
-            <input type="file" name="fileToUpload" id="fileToUpload" onchange="handleFileSelection()"><br>
-            <input type="text" name="fileName" id="fileName" placeholder="Введите название файла" disabled><br>
-            <input type="submit" value="Загрузить файл" name="submit" id="submitBtn" disabled>
-        </form>
+        <div class="message">
+            <h1>Software that just works</h1>
 
-        <?php
 
-        ?>
+        </div>
     </div>
-
-    <script>
-        function handleFileSelection() {
-            var fileInput = document.getElementById('fileToUpload');
-            var fileNameInput = document.getElementById('fileName');
-            var submitBtn = document.getElementById('submitBtn');
-
-            if (fileInput.files.length > 0) {
-                fileNameInput.disabled = false;
-                fileNameInput.focus(); 
-            } else {
-                fileNameInput.disabled = true;
-                submitBtn.disabled = true;
-            }
-        }
-
-        document.getElementById('fileName').addEventListener('input', function() {
-            var submitBtn = document.getElementById('submitBtn');
-            if (this.value.trim() !== '') {
-                submitBtn.disabled = false;
-            } else {
-                submitBtn.disabled = true;
-            }
-        });
-    </script>
 </body>
 </html>
